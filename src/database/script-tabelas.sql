dcb8ed10ba7985fcf69ae6ac9489783f67829f78
Gustavo Gil <gustavo.gil@sptech.school>
2024-05-14 10:09:25 -0300

    alteração das tabelas

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -4,14 +4,17 @@ CREATE DATABASE parkour;
 
 USE parkour;
 
+CREATE TABLE modalidade (
+	idModalidade INT PRIMARY KEY AUTO_INCREMENT,
+	modalidade VARCHAR(45)
+);
+
 CREATE TABLE usuario (
 	idUsuario INT PRIMARY KEY AUTO_INCREMENT,
 	nomeUsuario VARCHAR(45),
 	emailUsuario VARCHAR(45) UNIQUE,
-	senhaUsuario VARCHAR(45)
+	senhaUsuario VARCHAR(45),
+	fkModalidade int, constraint fkMod FOREIGN KEY (fkModalidade) REFERENCES modalidade(idModalidade)
 );
 
 SELECT * from usuario;
-
-select idUsuario, nomeUsuario, senhaUsuario from usuario where emailUsuario = 'gustavo@gmail.com';
-
